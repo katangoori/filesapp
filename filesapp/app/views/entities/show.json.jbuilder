16c78937abaa06d0dbafcaa3bdc91f324ba69e48
Nishanth Reddy <Your Name>
2017-09-19 22:09:11 -0400

    pp

---
+++ filesapp/app/views/entities/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @entity, :id, :name, :entity_type, :entity_id, :created_at, :updated_at
---
+++ filesapp/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_inmem_file_str_app_session'
